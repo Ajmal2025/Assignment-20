d4662d4469d7c83db0d08351e733afb6faf0a532
Ajmal2025 <139298518+Ajmal2025@users.noreply.github.com>
2023-10-25 22:07:02 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+// 20.	Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+//  generic array type Array<elementtype>
+let mountain = ["k2", "Mount Everest", "Rackaposhi"];
+let rivers = ["Nile", "Indus", "Ravi"];
+let language = ["Arabic", "Urdu", "English"];
+let countries = ["Pakistan", "Turkey", "Saudi Arabia"];
+let city = ["Lahore", "Sahiwal", "Pakpattan"];
+// \n to go to next line
+console.log(`${mountain} \n ${rivers} \n ${language} \n ${countries} \n ${city}`);
---
+++ index.ts
@@ -0,0 +1,12 @@
+
+
+// 20.	Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+//  generic array type Array<elementtype>
+let mountain:Array<string>=["k2","Mount Everest","Rackaposhi"];
+let rivers:Array<string>=["Nile","Indus","Ravi"];
+let language:Array<string>=["Arabic","Urdu","English"];
+let countries:Array<string>=["Pakistan","Turkey","Saudi Arabia"];
+let city:Array<string>=["Lahore","Sahiwal","Pakpattan"];
+// \n to go to next line
+console.log(`${mountain} \n ${rivers} \n ${language} \n ${countries} \n ${city}`)
